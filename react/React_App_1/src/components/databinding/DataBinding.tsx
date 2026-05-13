921603dfa26e31f509ad243ba766b10145a19f33
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-05-13 22:14:33 +0530

    may 13

--- react/React_App_1/src/components/databinding/DataBinding.tsx
+++ react/React_App_1/src/components/databinding/DataBinding.tsx
@@ -1,16 +1,51 @@
-import React, { Component } from "react";
-
-/* export default class DataBinding extends Component {
-  render() {
-    return (
-      <div>databinding</div>
-    )
-  }
-} */
+import { useState } from "react";
 
 export default function DataBinding() {
-  return <>
-          <div>Let's learn DataBinding</div>
-          <div>This is databinding Component</div>
-       </>
+  let count = 0; // Not a State Variable ( Local Variable)
+  const increment = () => {
+    count = count + 1;
+    console.log(`Count: ${count}`);
+  };
+
+  let [myCount, updateMyCount] = useState(0); // State Variable
+  const incrementCount = () => {
+    updateMyCount(myCount++);
+  };
+
+  let [name, setName] = useState("Sanjay"); // name is state variable
+
+  let [num, setNum] = useState(2); // num is state variable
+
+  return (
+    <>
+      <h3 className="text-center">Lets learn Data Binding</h3>
+
+      <div className="border border-3 rounded-3 p-3 m-3">
+        <div>Local variable count:{count}</div>
+        <button className="btn btn-primary" onClick={increment}>
+          Increment
+        </button>
+      </div>
+
+      <div className="border border-3 rounded-3 p-3 m-3">
+        <div>State variable MyCount:{myCount}</div>
+        <button className="btn btn-primary" onClick={incrementCount}>
+          Increment
+        </button>
+      </div>
+
+      <div className="border border-3 rounded-3 p-3 m-3">
+        <div>2 way Binding:</div>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <div>Name is(1-way):{name}</div>
+      </div>
+
+      <div className="border border-3 rounded-3 p-3 m-3">
+        <div>Even or Odd::</div>
+        <input type="number" value={num} onChange={(e) => setNum(+e.target.value)} />
+        
+        { num%2 ==0 ? <div>{num} is even Number</div> : <div>{num} is odd Number</div>}
+      </div>
+    </>
+  );
 }
