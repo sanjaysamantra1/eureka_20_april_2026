b91de90a2569a3c1be33115288257699cba9a02d
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-05-19 21:16:11 +0530

    function as props

--- react/React_App_1/src/components/body/Body.tsx
+++ react/React_App_1/src/components/body/Body.tsx
@@ -15,6 +15,9 @@ function Body() {
   console.log(sub(10, 20));
   console.log(mul(10, 20));
   console.log(React); */
+  const f1FromBody = ()=>{
+    console.log('I am f1 from Body Component')
+  }
 
   return (
     <div className="myBody" style={{ minHeight: "300px" }}>
@@ -27,18 +30,18 @@ function Body() {
       {/* <ProductList></ProductList> */}
       {/* <EmployeeList /> */}
       {/* <TaskList /> */}
-      {/* <PropsDemo1 name="Sanjay" msg="Good Morning" >
+      <PropsDemo1 name="Sanjay" msg="Good Morning" f1FromBody={f1FromBody} >
           <h3>Dynamic Content-1 From first Parent</h3>
           <h3>Dynamic Content-2 From first Parent</h3>
       </PropsDemo1>
       <hr />
-      <PropsDemo1 name="Sanjay" msg="Good Morning" >
+      {/* <PropsDemo1 name="Sanjay" msg="Good Morning" >
           <h3>Dynamic Content-1 From second Parent</h3>
           <h3>Dynamic Content-2 From second Parent</h3>
       </PropsDemo1> */}
-      <MyModal>
+      {/* <MyModal>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s' />
-      </MyModal>
+      </MyModal> */}
     </div>
   );
 }
--- react/React_App_1/src/components/props/PropsDemo1.tsx
+++ react/React_App_1/src/components/props/PropsDemo1.tsx
@@ -3,13 +3,16 @@ interface Greeting{
   name:string; // mandatory props
   msg?:string; // optional props
   children?:any;
+  f1FromBody?:any;
 }
-export default function PropsDemo1({ name, msg = 'good morning',children}:Greeting) {
+export default function PropsDemo1({ name, msg = 'good morning',children,f1FromBody}:Greeting) {
   return <>
   <h3>Hello, {name} -- {msg}</h3>
   {/* <h3>Static content-1 from Child Component</h3>
   <h3>Static content-2 from Child Component</h3> */}
   {children}
+
+  <button className="btn btn-primary" onClick={f1FromBody}>invoke f1</button>
   </>
 }
 
