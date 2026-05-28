17c80e5cf56e73da470611f089e631852112b23c
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-05-28 20:58:00 +0530

    form with validation

--- react/React_App_1/src/components/form/FormDemo1.tsx
+++ react/React_App_1/src/components/form/FormDemo1.tsx
@@ -3,38 +3,45 @@ import { useState } from "react";
 export default function FormDemo1() {
   const defaultUserData = {
     firstName: "Sanjay",
+    lastName: "samantr",
+    email: "",
+    salary: "",
+    phone: "",
+  };
+
+  const defaultErrors = {
+    firstName: "",
     lastName: "",
     email: "",
-    salary: 0,
-    phone: 0,
+    salary: "",
+    phone: "",
   };
-  const [user, setUser] = useState(defaultUserData);
-  const [errors, setErrors] = useState({
-    firstName: null,
-    lastName: null,
-    email: null,
-    salary: null,
-    phone: null,
-  });
 
-  const changeHandler = (e:any)=>{
+  const [user, setUser] = useState(defaultUserData);
+  const [errors, setErrors] = useState(defaultErrors);
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const fieldName = e.target.name;
     const fieldValue = e.target.value;
-    setUser({...user , [fieldName]:fieldValue});
 
-    if(fieldValue == ''){
-      setErrors({...errors, [fieldName]:`${fieldName} is required`})
-    }else{
-      setErrors({...errors, [fieldName]:''})
-    }
-  }
-  const submitMyForm = (e:any)=>{
+    setUser({ ...user, [fieldName]: fieldValue });
+
+    setErrors({
+      ...errors,
+      [fieldName]:fieldValue.trim() === "" ? `${fieldName} is required`: "",
+    });
+  };
+
+  const submitMyForm = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log('Form Submitted', user)
-  }
-  const resetMyForm = ()=>{
-    setUser({...defaultUserData});
-  }
+    console.log("Form Submitted", user);
+  };
+
+  const resetMyForm = () => {
+    setUser(defaultUserData);
+    setErrors(defaultErrors);
+  };
+
   return (
     <>
       <div className="col-sm-4 offset-4 border border-3 rounded-3 p-3">
@@ -42,31 +49,37 @@ export default function FormDemo1() {
 
         <form onSubmit={submitMyForm} onReset={resetMyForm}>
           <p>
-            FirstName: <input name='firstName' value={user.firstName} onChange={changeHandler} required />
+            FirstName: <input name="firstName" value={user.firstName} onChange={changeHandler} />
             <span className="text-danger">{errors.firstName}</span>
           </p>
+
           <p>
-            lastName: <input name='lastName' value={user.lastName} onChange={changeHandler} />
+            LastName: <input name="lastName" value={user.lastName} onChange={changeHandler} />
             <span className="text-danger">{errors.lastName}</span>
           </p>
+
           <p>
-            Email: <input name='email' type="email" value={user.email} onChange={changeHandler} required />
+            Email: <input name="email" type="email" value={user.email} onChange={changeHandler} />
             <span className="text-danger">{errors.email}</span>
           </p>
+
           <p>
-            Salary: <input name='salary' value={user.salary} onChange={changeHandler} />
+            Salary: <input name="salary" value={user.salary} onChange={changeHandler} />
+            <span className="text-danger">{errors.salary}</span>
           </p>
+
           <p>
-            Phone: <input name='phone' value={user.phone} onChange={changeHandler} />
+            Phone: <input name="phone" value={user.phone} onChange={changeHandler} />
+            <span className="text-danger">{errors.phone}</span>
           </p>
 
           <input type="reset" value="Reset" />
           <input type="submit" value="Submit" />
         </form>
-
       </div>
-    <pre>{JSON.stringify(user)}</pre>
-    <pre>{JSON.stringify(errors)}</pre>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <pre>{JSON.stringify(errors, null, 2)}</pre>
     </>
   );
 }
